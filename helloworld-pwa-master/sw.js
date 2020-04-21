e60fb4d023fd1827e9fef8d4c0424fbba5ed899d
jeffersoncf <63351248+jeffersoncf@users.noreply.github.com>
2020-04-20 22:07:39 -0300

    Initial commit

---
+++ helloworld-pwa-master/sw.js
@@ -0,0 +1,31 @@
+var cacheName = 'hello-pwa'
+
+var filesToCache = [
+    '/',
+    '/index.html',
+    '/css/style.css',
+]
+
+self.addEventListener('install', (e) => {
+    e.waitUntil(
+        caches.open(cacheName).then((cache)=>{
+            return cache.addAll(filesToCache)
+        })
+    )
+})
+
+self.addEventListener('fetch', (e) => {
+    e.respondWith(
+        caches.match(e.request).then(()=>{
+            return response || fetch(e.request)
+        })
+    )
+})
+
+window.onload = () => {
+    'use strict'
+
+    if('serviceWorker' in navigator) {
+        navigator.serviceWorker.register('.sw.js')
+    }
+}
